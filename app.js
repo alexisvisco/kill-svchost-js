76302ea12487e37f6f292245ae3bcbbfcb478a03
Alexis Viscogliosi <kwizzy@outlook.fr>
2017-10-30 21:30:39 +0100

    Update app.js
    Take real pid of svchost

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 const { exec } = require('child_process')
 
-console.log("HEY WE ARE GOING TO KILL THIS SVCHOST !! ")
+console.log("-- Now you have 100% of your internet connection --")
 
-function getMatches(string, regex, index = 1) {
-	const matches = []
-	let match
-	while (match = regex.exec(string)) matches.push(match[index])
-	return matches;
+function getMatches(string, regex, index) {
+	index || (index = 1);
+	var matches = [];
+	var match;
+	while (match = regex.exec(string)) {
+		return match[index];
+	}
 }
 
-setInterval(() => {
-	exec(`netstat -b -o -n`, (error, stdout, stderr) => {
-		const out = stdout.split("\n")
-		for (let i = 0; i < out.length; i++) { 
-			const e = out[i];
-			if (e.includes("[svchost.exe]")) {
-				let pid = out[i + 1].split(" ")
-				pid = pid[pid.length-1].replace("\r", "")
+function getPid(array, i) {
+	while (array[i--])
+		if (array[i].includes("TCP"))
+			return +getMatches(array[i] + '@', /(\d+@)/).replace('@', '')
+}
 
-				if (pid && +pid > 0)
-				{
-					console.log("kill process with name " + e + " and pid : " + pid)
-					exec("taskkill /F /PID " + pid, (er, ou, serr) => console.log(ou))		
-					break;
-				}
+const delSvchost = () => exec(`netstat -b -o -n`, (error, stdout, stderr) => {
+	const out = stdout.split("\n")
+	for (let i = 0; i < out.length; i++) {
+		const e = out[i];
+		if (e.includes("svchost.exe")) {
+			let pid = getPid(out, i)
+			console.log(pid)
+			if (pid) {
+				console.log("Kill process with pid " + pid + ".")
+				exec("taskkill /F /PID " + pid, (er, ou, serr) => { })
+				break;
 			}
 		}
-	})
-}, 3000)
+	}
+})
 
-
+setInterval(() => {
+	delSvchost()
+}, 8000)
